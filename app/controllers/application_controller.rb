f23a22f698688ab375f38577a703f6a2b1179035
boyxsh <boyxsh@gmail.com>
2011-03-24 22:03:37 +0800

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,7 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+  def after_sign_in_path_for(resource_or_scope)
+       return rails_admin_dashboard_path 
+     end
+    
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BotanicalGarden::Application.config.secret_token = '78f980756d55121068eb1c713b9b76232fcd016b939f33ead40d6fc98a0db473d376d20740570ef304023b9203b4af26eebcc5b41cfc7e6da752f04c453fa897'
