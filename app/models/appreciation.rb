2d4611feb1ac541c4b7effdbdd900a52bf9164a5
boyxsh <boyxsh@gmail.com>
2011-05-22 20:48:58 +0800

    make some UI

--- app/models/appreciation.rb
+++ app/models/appreciation.rb
@@ -1,5 +1,5 @@
 class Appreciation < ActiveRecord::Base
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }#,:xsh=>"50x50>"}
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "150x150>" }#,:xsh=>"50x50>"}
 
   belongs_to :category
 
