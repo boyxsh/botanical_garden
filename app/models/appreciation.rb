b773eb32b1c035f5ade56c925f6bff6f060ed455
boyxsh <boyxsh@gmail.com>
2011-05-24 01:23:00 +0800

    add appreciations search; appreciations filter; jquery lazyload

--- app/models/appreciation.rb
+++ app/models/appreciation.rb
@@ -2,5 +2,6 @@ class Appreciation < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "150x150>" }#,:xsh=>"50x50>"}
 
   belongs_to :category
+  scope :search_by, lambda { |search| { :conditions => ['name like ?', "%#{search}%"] } }
 
 end
